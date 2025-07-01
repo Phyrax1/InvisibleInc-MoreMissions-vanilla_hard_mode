7389d2758142896dda132a0ad95f66ae8eebf1fd
Phyrax1 <124089724+Phyrax1@users.noreply.github.com>
2025-07-01 14:46:01 +0200

    Added cxt.maxUpdatedPlacement[tag]

--- scripts/appended_functions/prefabs.lua
+++ scripts/appended_functions/prefabs.lua
@@ -24,6 +24,37 @@ prefabs.generatePrefabs = function( cxt, candidates, tag, maxCount, fitnessFn, f
 		-- for i = (oldMaxCount - 1), 1, -1 do
 			-- generatePrefabs_old( cxt, candidates, tag, maxCount, fitnessFn, fitnessSelect, ... ) --needed so BOTH guard exits spawn far from mole prefab. Re-run function as many times as original maxCount
 		-- end
+		        -- used for cfo office so only one guard entry gets adjusted, does custom fitness for maxUpdatedPlacement times, then default to vanilla
+        	if cxt.maxUpdatedPlacement and cxt.maxUpdatedPlacement[tag] then
+            		local limit = cxt.maxUpdatedPlacement[tag]
+            		local placed = {}
+            		local nSmart = math.min(limit, maxCount)
+
+            		local smart = {generatePrefabs_old(cxt, candidates, tag, nSmart, fitnessFn, fitnessSelect, ...)}
+            		for _, inst in ipairs(smart) do
+                		table.insert(placed, inst)
+            		end
+
+            		cxt.maxUpdatedPlacement[tag] = limit - nSmart
+
+            		local remain = maxCount - nSmart
+            		if remain > 0 then
+                		local oldFn = fitnessFn
+                		local oldSelect = fitnessSelect
+
+                		fitnessFn = nil
+                		fitnessSelect = nil
+
+                		local dumb = {generatePrefabs_old(cxt, candidates, tag, remain, fitnessFn, fitnessSelect, ...)}
+                		for _, inst in ipairs(dumb) do
+                    			table.insert(placed, inst)
+                		end
+
+                		fitnessFn = oldFn
+                		fitnessSelect = oldSelect
+            		end
+            		return placed
+        	end
 	end
 
 	return generatePrefabs_old( cxt, candidates, tag, maxCount, fitnessFn, fitnessSelect, ... )
