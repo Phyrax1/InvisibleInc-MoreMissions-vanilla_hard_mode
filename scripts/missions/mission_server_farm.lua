85bf25a58543f03adddd93337ef52e382b2fd156
Phyrax1 <124089724+Phyrax1@users.noreply.github.com>
2025-07-11 21:21:34 +0200

    Cleanup

--- scripts/missions/mission_server_farm.lua
+++ scripts/missions/mission_server_farm.lua
@@ -1,3 +1,6 @@
+-- hard mode: spawn 4 2.0 daemons instead of 3 normal ones
+-- TODO: check if all 2.0 daemons actually work correctly when installed on devices/stacked with others or themselves
+---------------------------------------------------------------------------------------------------------------
 local mission = include("sim/missions/mission_server_farm")
 local simdefs = include("sim/simdefs")
 local mission_util = include("sim/missions/mission_util")
@@ -5,20 +8,17 @@ local SCRIPTS = include('client/story_scripts')
 local serverdefs = include("modules/serverdefs")
 local cdefs = include("client_defs")
 
-local USE_TERM = {
-    action = "abilityAction",
-    fn = function(sim, ownerID, userID, abilityIdx, ...)
-        local unit, ownerUnit = sim:getUnit(userID), sim:getUnit(ownerID)
+local USE_TERM = {action = "abilityAction", fn = function(sim, ownerID, userID, abilityIdx, ...)
+    local unit, ownerUnit = sim:getUnit(userID), sim:getUnit(ownerID)
 
-        if not unit or not unit:isPC() or not ownerUnit or not ownerUnit:getTraits().bigshopcat then
-            return nil
-        end
-
-        if ownerUnit:getAbilities()[abilityIdx]:getID() == "showItemStore" then
-            return ownerUnit
-        end
+    if not unit or not unit:isPC() or not ownerUnit or not ownerUnit:getTraits().bigshopcat then
+        return nil
     end
-}
+
+    if ownerUnit:getAbilities()[abilityIdx]:getID() == "showItemStore" then
+        return ownerUnit
+    end
+end}
 
 local function useServerTerminal(script, sim, mission)
     local _, terminal = script:waitFor(USE_TERM)
@@ -26,13 +26,13 @@ local function useServerTerminal(script, sim, mission)
     script:waitFor(mission_util.UI_SHOP_CLOSED)
 
     terminal:destroyTab()
-    --------------------------------------------------------------------------------------------
+    --[[ --------------------------------------------------------------------------------------------
     -- daemon on terminal use
     script:waitFrames(.5 * cdefs.SECONDS)
     sim:getNPC():addMainframeAbility(
         sim, serverdefs.ENDLESS_DAEMONS[sim:nextRand(1, #serverdefs.ENDLESS_DAEMONS)], sim:getNPC(), 0
     )
-    --------------------------------------------------------------------------------------------
+    -------------------------------------------------------------------------------------------- ]]
     local possibleUnits = {}
     for _, unit in pairs(sim:getAllUnits()) do
         if unit:getTraits().mainframe_item and unit:getPlayerOwner() ~= sim:getPC()
@@ -43,15 +43,12 @@ local function useServerTerminal(script, sim, mission)
     if #possibleUnits > 0 then
         script:waitFrames(.75 * cdefs.SECONDS)
         script:queue(
-            {
-                type = "showIncognitaWarning",
-                txt = STRINGS.UI.WARNING_NEW_DAEMON,
-                vo = "SpySociety/VoiceOver/Incognita/Pickups/Warning_New_Daemon"
-            }
+            {type = "showIncognitaWarning", txt = STRINGS.UI.WARNING_NEW_DAEMON,
+             vo = "SpySociety/VoiceOver/Incognita/Pickups/Warning_New_Daemon"}
         )
         script:waitFrames(.75 * cdefs.SECONDS)
 
-        for k = 1, 3 do
+        for k = 1, 4 do -- increased to 4
             if #possibleUnits > 0 then
                 local index = sim:nextRand(1, #possibleUnits)
                 local unit = possibleUnits[index]
@@ -63,34 +60,15 @@ local function useServerTerminal(script, sim, mission)
                                                      )]
                 ---------------------------------------------------------------------------------------------
                 local x, y = unit:getLocation()
-                script:queue(
-                    {
-                        type = "pan",
-                        x = x,
-                        y = y
-                    }
-                )
+                script:queue({type = "pan", x = x, y = y})
 
                 sim:getPC():glimpseUnit(sim, unit:getID())
+                sim:dispatchEvent(simdefs.EV_UNIT_MAINFRAME_UPDATE, {units = {unit.unitID}, reveal = true})
                 sim:dispatchEvent(
-                    simdefs.EV_UNIT_MAINFRAME_UPDATE, {
-                        units = {unit.unitID},
-                        reveal = true
-                    }
+                    simdefs.EV_UNIT_UPDATE_ICE,
+                    {unit = unit, ice = unit:getTraits().mainframe_ice, delta = 0, refreshAll = true}
                 )
-                sim:dispatchEvent(
-                    simdefs.EV_UNIT_UPDATE_ICE, {
-                        unit = unit,
-                        ice = unit:getTraits().mainframe_ice,
-                        delta = 0,
-                        refreshAll = true
-                    }
-                )
-                sim:dispatchEvent(
-                    simdefs.EV_MAINFRAME_INSTALL_NEW_DAEMON, {
-                        target = unit
-                    }
-                )
+                sim:dispatchEvent(simdefs.EV_MAINFRAME_INSTALL_NEW_DAEMON, {target = unit})
                 sim:dispatchEvent(simdefs.EV_PLAY_SOUND, "SpySociety/Actions/mainframe_daemonmove")
                 script:waitFrames(1 * cdefs.SECONDS)
             end
@@ -98,34 +76,26 @@ local function useServerTerminal(script, sim, mission)
 
         local x, y = terminal:getLocation()
         sim:dispatchEvent(simdefs.EV_SCRIPT_EXIT_MAINFRAME)
-        script:queue(
-            {
-                type = "pan",
-                x = x,
-                y = y
-            }
-        )
+        script:queue({type = "pan", x = x, y = y})
         script:waitFrames(1 * cdefs.SECONDS)
         script:queue(
-            {
-                script = SCRIPTS.INGAME.AFTERMATH.SERVERFARM[sim:nextRand(1, #SCRIPTS.INGAME.AFTERMATH.SERVERFARM)],
-                type = "newOperatorMessage"
-            }
+            {script = SCRIPTS.INGAME.AFTERMATH.SERVERFARM[sim:nextRand(1, #SCRIPTS.INGAME.AFTERMATH.SERVERFARM)],
+             type = "newOperatorMessage"}
         )
 
         script:queue(1 * cdefs.SECONDS)
         script:queue(
-            {
-                script = SCRIPTS.INGAME.MONSTERCAT_POST[sim:nextRand(1, #SCRIPTS.INGAME.MONSTERCAT_POST)],
-                type = "newOperatorMessage"
-            }
+            {script = SCRIPTS.INGAME.MONSTERCAT_POST[sim:nextRand(1, #SCRIPTS.INGAME.MONSTERCAT_POST)],
+             type = "newOperatorMessage"}
         )
     end
 end
 
+---------------------------------------------------------------------------------------------------------------
+
 local oldmissioninit = mission.init
 function mission:init(scriptMgr, sim)
-    local rets = {oldmissioninit(self, scriptMgr, sim)}
+    oldmissioninit(self, scriptMgr, sim)
     local diffOpts = sim:getParams().difficultyOptions
     if diffOpts.MM_difficulty and diffOpts.MM_difficulty == "hard" then
         for _, hook in pairs(scriptMgr.hooks) do
@@ -136,5 +106,4 @@ function mission:init(scriptMgr, sim)
         end
         scriptMgr:addHook("USE_TERMINAL", useServerTerminal, nil, self)
     end
-    return unpack(rets)
 end
